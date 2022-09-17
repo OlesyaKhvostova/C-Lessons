f008704dd514ca0d8b3a280360d81d963c78460d
olesya <olesya_khvostova@mail.ru>
2022-09-17 07:47:08 +0300

    Add Example and add gitignore file

---
+++ example1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Прием у врача");
+            Console.WriteLine("Покупка лекарств");
+            Console.WriteLine("Прогулка в парке");
+            Console.Write("Вот так я провела свой день");
+            Console.Write(name);
+        }
+    }
+}
