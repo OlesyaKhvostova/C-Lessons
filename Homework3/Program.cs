e3bb8379c084dd8be1969e797c1b220d66948890
olesya <olesya_khvostova@mail.ru>
2022-09-18 16:39:17 +0300

    add homework3

---
+++ Homework3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ввести число");
+int value = Convert.ToInt32(Console.ReadLine());
+if (value % 2 == 0)
+{
+    Console.WriteLine($"{value} это четное число");
+}
+else
+{
+    Console.WriteLine($"{value} это нечетное число");
+}
