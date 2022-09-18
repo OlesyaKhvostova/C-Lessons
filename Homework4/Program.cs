2cc37289214191a074386f2552f84acc029a1cc5
olesya <olesya_khvostova@mail.ru>
2022-09-18 17:18:13 +0300

    add hoework4

---
+++ Homework4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Ввести число");
+int value = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{value} ->");
+for(int i = 1; i <= value; i++)
+{
+   
+    if (i % 2 == 0)
+    {
+        Console.Write($" {i}");
+
+    }
+
+}
+Console.Write($"\n{value} ->");
+int count = 1;
+while (count <= value)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($" {count}");
+    }
+    count++;
+
+}
