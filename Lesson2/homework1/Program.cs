e7b7d7fc4cf818b587763d396e4454c0af1cdb27
olesya <olesya_khvostova@mail.ru>
2022-09-20 23:16:57 +0300

    add homework1 Lesson2

---
+++ Lesson2/homework1/Program.cs
@@ -0,0 +1,22 @@
+﻿int Calculate(int value)
+{
+    int remainValue = value % 100;
+    int smallValue = remainValue % 10;
+    int result = (remainValue - smallValue)/10;
+    return result;
+} 
+
+Console.WriteLine("Введите трехзначное число");
+int value = Convert.ToInt32(Console.ReadLine());
+if (value > 99 && value < 1000)
+{
+    int result = Calculate(value);
+    Console.WriteLine($"{value} -> {result}");
+} 
+else
+{
+    Console.WriteLine("Число неккоректное");
+}
+
+
+
