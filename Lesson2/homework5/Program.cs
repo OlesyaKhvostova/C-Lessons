b7093488416ebe111ece80397dc180475ac3f2fd
olesya <olesya_khvostova@mail.ru>
2022-09-24 09:45:02 +0300

    Add Lesson2 Homework5

---
+++ Lesson2/homework5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введенное число {0}", value);
+int randomValue = new Random().Next(1,100);
+while((randomValue % value) != 0)
+{
+    Console.WriteLine("{0} -> не кратно", randomValue);
+    randomValue = new Random().Next(1,100);
+}
+
+Console.WriteLine("{0} -> кратно", randomValue);
