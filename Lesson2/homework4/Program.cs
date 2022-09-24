30cb0b052c011f4b3c3e622b10cc4503f311a03c
olesya <olesya_khvostova@mail.ru>
2022-09-24 09:32:11 +0300

    Add Lesson2 Homework4

---
+++ Lesson2/homework4/Program.cs
@@ -0,0 +1,9 @@
+﻿var weekDays = new string[]{"Рабочий", "Рабочий", "Рабочий", "Рабочий", "Рабочий", "Выходной", "Выходной"};
+Console.WriteLine("Введите день недели");
+int day = Convert.ToInt32(Console.ReadLine());
+string answer = "Нет такого дня";
+if ( day > 0 && day <= 7)
+{
+    answer = weekDays[day - 1];
+}
+Console.WriteLine("{0} -> {1}", day, answer);
