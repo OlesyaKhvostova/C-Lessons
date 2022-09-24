5c6fd402fd5faa0bc71b7c6c6cae8ff42200f932
olesya <olesya_khvostova@mail.ru>
2022-09-24 09:20:39 +0300

    Add Lesson2 Homework3

---
+++ Lesson2/homework3/Program.cs
@@ -0,0 +1,28 @@
+﻿int ConvertValue(int value)
+{
+    int maxOrder = 1;
+
+    while ((value / Convert.ToInt32(Math.Pow(10, maxOrder))) > 0)
+    {
+        maxOrder++;
+    }
+
+    int[] digits = new int[maxOrder];
+
+    for (int i = maxOrder - 1, index = 0; i >= 0; i--, index++)
+    {
+        int divider = Convert.ToInt32(Math.Pow(10, i));
+        int temp = value % divider;
+        int digit = (value - temp)/divider;
+        digits[index] = digit;
+        
+        value = temp;
+    }
+
+    return digits.Length >= 3 ? digits[2] : -1;
+}
+
+Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+int result = ConvertValue(value);
+Console.WriteLine("{0}", result == -1 ? "третьего числа нет" : result);
