b72e02df23387adb0c1c2da4a4d2306e2985ae56
olesya <olesya_khvostova@mail.ru>
2022-09-25 13:11:57 +0300

    Add Lesson3 Homework3

---
+++ Lesson3/homework3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{value} ->");
+for (int i = 1; i <= value; i++)
+{
+    Console.Write("{0}{1}", Math.Pow(i,3), i < value ? ", " : "");
+}
