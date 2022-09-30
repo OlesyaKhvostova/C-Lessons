ec28633576e5095f4cd18c7ccf33100c62c72050
olesya <olesya_khvostova@mail.ru>
2022-09-30 07:22:10 +0300

    Add Lesson4 Homework1

---
+++ Lesson4/homework1/Program.cs
@@ -0,0 +1,16 @@
+﻿int CalcPower(int value, int power)
+{
+    int result = value;
+    for(int i = 1; i < power; i++)
+    {
+        result *= value;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа");
+int power = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Число {0} в степени {1} -> {2}", value, power, CalcPower(value, power));
