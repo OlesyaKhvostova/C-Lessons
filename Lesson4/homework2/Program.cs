54dacb37bb5e8f15006d56a5ef13928554398939
olesya <olesya_khvostova@mail.ru>
2022-09-30 10:29:21 +0300

    Add Lesson4 homework2

---
+++ Lesson4/homework2/Program.cs
@@ -0,0 +1,21 @@
+﻿int Summ(int value)
+{
+    int result = 0;
+    
+    for (int i = 10; i >= 0 ; i--)
+    {
+        int powerValue = (int)Math.Pow(10, i);
+        if (value >= powerValue)
+        {
+            int part = value % powerValue;
+            result += (value - part)/powerValue;
+            value = part;
+        } 
+    }
+
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("{0} -> {1}", value, Summ(value));
