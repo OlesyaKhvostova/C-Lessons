60137f2bcc61cf3597dc2b49bc85cb1fafb883fa
olesya <olesya_khvostova@mail.ru>
2022-10-02 00:09:04 +0300

    add homework2_1

---
+++ Lesson2/homework2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int Calculate(int value)
+{
+    int result = 0;
+    int remainValue = value % 100;
+    int firstNumber = (value - remainValue)/100;
+    result = firstNumber * 10;
+    int thirdNumber = value % 10;
+    result += thirdNumber;
+    return result;
+}
+
+
+int value = new Random().Next(100, 1000);
+Console.WriteLine("{0} -> {1}",value, Calculate(value));
+
