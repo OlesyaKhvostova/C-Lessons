d2a02c1c168831b3365afd5c099e7e57b068cc0e
olesya <olesya_khvostova@mail.ru>
2022-10-01 11:18:57 +0300

    Add Lesson4 homework4

---
+++ Lesson4/Homework4/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] GenerateArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    return array;
+}
+
+int GetSecondMax(int[] array)
+{
+    int maxIndex = 0;
+    int index = -1;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > array[maxIndex])
+        {
+            index = maxIndex;
+            maxIndex = i;
+        }
+        else if (array[i] != array[maxIndex])
+        {
+            if (index == -1 || array[index] < array[i])
+            {
+                index = i;
+            }
+        }
+    }
+
+    return index;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write("{0} ", array[i]);
+    }
+    Console.Write("->");
+    int index = GetSecondMax(array);
+    Console.Write(" {0}", index == -1 ? "не найден" : array[index]);
+}
+
+PrintArray(GenerateArray());
