76862e5b1a4c92d8b70392f89d4f9912d4126590
olesya <olesya_khvostova@mail.ru>
2022-10-08 01:03:22 +0300

    Add Lesson6 Homework1

---
+++ Lesson6/homework1/Program.cs
@@ -0,0 +1,17 @@
+﻿int CalculateNegativeDigitCount(string digits)
+{
+    string[] array = digits.Split(',');
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (Convert.ToInt32(array[i]) > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine("введите набор положительных и отрицательных чисел через запятую");
+string digits = Console.ReadLine();
+
+Console.WriteLine("Пользователь ввел {0} чисел больше 0", CalculateNegativeDigitCount(digits));
