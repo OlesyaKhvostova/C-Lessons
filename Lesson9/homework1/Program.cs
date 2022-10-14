5f0adf56466cfc6df3aef7d69b4c99e25ce3e769
olesya <olesya_khvostova@mail.ru>
2022-10-14 23:56:55 +0300

    Add Lesson9 homework1

---
+++ Lesson9/homework1/Program.cs
@@ -0,0 +1,12 @@
+﻿void PrintDigit(int number)
+{
+    if( number > 0)
+    {
+        Console.Write($"{number} ");
+        PrintDigit(number - 1);
+    }
+}
+
+Console.WriteLine("Введите число больше 0");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintDigit(number);
