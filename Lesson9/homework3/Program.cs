eec66829c97c3055f9801fb8d668667f11a80467
olesya <olesya_khvostova@mail.ru>
2022-10-15 00:17:39 +0300

    add Lesson9 homework3

---
+++ Lesson9/homework3/Program.cs
@@ -0,0 +1,28 @@
+﻿uint FunctionA(uint numberN, uint numberM)
+{
+    if (numberN == 0)
+    {
+        return numberM + 1;
+    }
+    else
+    {
+        if ((numberN != 0) && (numberM == 0))
+        {
+            return FunctionA(numberN - 1, 1);
+        }
+        else
+        {
+            return FunctionA(numberN - 1, FunctionA(numberN, numberM - 1));    
+        }
+    }
+}
+
+
+Console.WriteLine("Введите 1 число больше 0");
+uint numberN = Convert.ToUInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2 число больше 0");
+uint numberM = Convert.ToUInt32(Console.ReadLine());
+
+uint summ = FunctionA(numberN, numberM);
+Console.WriteLine($"{summ}");
