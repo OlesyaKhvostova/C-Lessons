8b58f418b765197670181c32eee1401e6f652284
olesya <olesya_khvostova@mail.ru>
2022-10-15 00:05:51 +0300

    Add Lesson9 homework2

---
+++ Lesson9/homework2/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetSumm(int numberM, int numberN)
+{
+    if ( numberM == numberN)return numberM;
+    return numberM + GetSumm(numberM + 1, numberN);
+}
+
+Console.WriteLine("Введите 1 число больше 0");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2 число больше 0");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int summ = GetSumm(numberM, numberN);
+Console.WriteLine($"{summ}");
