222434a5e32bae377d88e756e04884fc645292e6
olesya <olesya_khvostova@mail.ru>
2022-10-31 22:31:51 +0300

    add my lesson2 homework3

---
+++ Lesson2/homework3_1/Program.cs
@@ -0,0 +1,14 @@
+﻿int Calculate(int value)
+{
+    int result = -1; 
+    if (value >= 100)
+    {
+        int temp = value % 1000;
+        result = (temp - temp % 100) / 100;
+    }
+    return result;
+}
+Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+int result = Calculate(value);
+Console.WriteLine("{0} -> {1}", value, result == -1 ? "Третьего числа не существует" : result);  
