56a9a8eb0412e5dfbf1407bb9671f08a7348849c
olesya <olesya_khvostova@mail.ru>
2022-11-05 20:40:18 +0300

    lesson2

---
+++ Lesson2/homework4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ввести день недели");
+int value = Convert.ToInt32(Console.ReadLine());
+if (value >= 1 && value <= 7)
+{
+    if (value >= 6)
+    {
+        Console.WriteLine($"{value} выходной день");
+    }
+    else if (value >= 1 && value <= 5)
+    {
+        Console.WriteLine($"{value} будний день");
+    }
+}
+else
+{
+    Console.WriteLine($"Значение {value} ошибка. Правильный диапазон от 1 до 7");
+}
