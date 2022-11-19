698f6d710a6bafb2b6e33d0b1171e382ce06bd1f
olesya <olesya_khvostova@mail.ru>
2022-11-19 21:06:25 +0300

    colculate

---
+++ Lesson2/test 5/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Ввести первое целочисленное число");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввести второе целочисленное число");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите операцию для этих чисел");
+string operation = Console.ReadLine();
+float total = 0.0f;
+if (operation == "+")
+{
+    total = firstValue + secondValue;   
+}
+
+if (operation == "-")
+{
+    total = firstValue - secondValue;
+}
+
+if (operation == "*")
+{
+    total = firstValue * secondValue;
+}
+
+if (operation == "/")
+{
+    total = firstValue / secondValue;
+}
+Console.WriteLine($"результат: {firstValue} {operation} {secondValue} = {total}");
